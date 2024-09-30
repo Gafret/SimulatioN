900ee6f848c76fcd40931041d1a4d73d6fe3063b
gafret <gafret_kelp@outlook.com>
2024-09-30 21:53:04 +0300

    refactor: change SearchAlgorithm interface

--- src/main/java/Simulation/searchalgorithms/AStarSearch.java
+++ src/main/java/Simulation/searchalgorithms/AStarSearch.java
@@ -2,10 +2,12 @@ package Simulation.searchalgorithms;
 
 import Simulation.entities.base.Entity;
 import Simulation.map.Coordinate2D;
+import Simulation.map.Map;
 
 public class AStarSearch implements SearchAlgorithm{
+
     @Override
-    public Coordinate2D findClosest(Entity entity) {
+    public Coordinate2D findClosest(Coordinate2D start, Class<? extends Entity> entityType, Map map) {
         return null;
     }
 }
--- src/main/java/Simulation/searchalgorithms/SearchAlgorithm.java
+++ src/main/java/Simulation/searchalgorithms/SearchAlgorithm.java
@@ -2,7 +2,8 @@ package Simulation.searchalgorithms;
 
 import Simulation.entities.base.Entity;
 import Simulation.map.Coordinate2D;
+import Simulation.map.Map;
 
 public interface SearchAlgorithm {
-    public Coordinate2D findClosest(Class<? extends Entity> entityType);
+    public Coordinate2D findClosest(Coordinate2D start, Class<? extends Entity> entityType, Map map);
 }
