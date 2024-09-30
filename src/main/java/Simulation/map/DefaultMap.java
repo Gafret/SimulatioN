1719fc3b356297f34aebc997bee4037f8ccedc93
gafret <gafret_kelp@outlook.com>
2024-09-30 20:42:00 +0300

    feat: basic food finding methods, not done, will return later

--- src/main/java/Simulation/map/DefaultMap.java
+++ src/main/java/Simulation/map/DefaultMap.java
@@ -1,4 +1,65 @@
 package Simulation.map;
 
-public class DefaultMap {
+import Simulation.entities.base.Entity;
+
+import java.util.HashMap;
+
+public class DefaultMap implements Map {
+    private final java.util.Map<Coordinate2D, Entity> map = new HashMap<Coordinate2D, Entity>();
+    private final int height;
+    private final int width;
+
+    public DefaultMap(int height, int width) {
+        this.height = height;
+        this.width = width;
+    }
+
+    @Override
+    public boolean addEntity(Entity entity, Coordinate2D position) {
+        if(!this.checkIsEmpty(position))
+            return false;
+
+        this.map.put(position, entity);
+        return true;
+    }
+
+    @Override
+    public boolean removeEntity(Coordinate2D position) {
+        if(this.checkIsEmpty(position))
+            return false;
+
+        this.map.remove(position);
+        return true;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return map.isEmpty();
+    }
+
+    @Override
+    public boolean moveEntity(Coordinate2D position, Coordinate2D destination) {
+        if(this.checkIsEmpty(position) || !this.checkIsEmpty(destination))
+            return false;
+        Entity entity = this.map.get(position);
+        this.removeEntity(position);
+        this.map.put(destination, entity);
+        return true;
+    }
+
+    @Override
+    public int getHeight() {
+        return this.height;
+    }
+
+    @Override
+    public int getWidth() {
+        return this.width;
+    }
+
+    private boolean checkIsEmpty(Coordinate2D position){
+        return !this.map.containsKey(position);
+    }
+
+    
 }
--- src/main/java/Simulation/map/Map.java
+++ src/main/java/Simulation/map/Map.java
@@ -1,4 +1,13 @@
 package Simulation.map;
 
+import Simulation.entities.base.Entity;
+
 public interface Map {
+    public boolean addEntity(Entity entity, Coordinate2D position);
+    public boolean removeEntity(Coordinate2D position);
+    public boolean isEmpty();
+    public boolean moveEntity(Coordinate2D position, Coordinate2D destination);
+
+    public int getHeight();
+    public int getWidth();
 }
