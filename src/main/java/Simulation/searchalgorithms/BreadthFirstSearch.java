5e8cecb820ca413496e2a79429bca8dca79683b6
gafret <gafret_kelp@outlook.com>
2024-10-01 11:55:25 +0300

    feat: add bfs

--- src/main/java/Simulation/searchalgorithms/BreadthFirstSearch.java
+++ src/main/java/Simulation/searchalgorithms/BreadthFirstSearch.java
@@ -4,10 +4,32 @@ import Simulation.entities.base.Entity;
 import Simulation.map.Coordinate2D;
 import Simulation.map.Map;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class BreadthFirstSearch implements SearchAlgorithm{
 
     @Override
     public Coordinate2D findClosest(Coordinate2D start, Class<? extends Entity> entityType, Map map) {
-        return null;
+        Queue<Coordinate2D> queue = new LinkedList<Coordinate2D>();
+        queue.add(start);
+        while(!queue.isEmpty()) {
+            Coordinate2D current = queue.remove();
+            int[][] directions = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
+            for(int[] direction : directions) {
+                int x = current.getX() + direction[0];
+                int y = current.getY() + direction[1];
+                if(x < 0 || x >= map.getWidth() || y < 0 || y >= map.getHeight()) {
+                    continue;
+                }
+                Coordinate2D newCoordinate = new Coordinate2D(x, y);
+                Entity entity = map.getEntity(newCoordinate);
+                if(entity != null && entity.getClass() == entityType) {
+                    return newCoordinate;
+                }
+                queue.add(newCoordinate);
+            }
+        }
+        return new Coordinate2D(-1, -1);
     }
 }
